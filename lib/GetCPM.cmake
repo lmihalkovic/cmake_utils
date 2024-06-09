44e1eff20dca4eee3b37886cbf1cc0c60a141173
Laurent M <laurentm@codingblues.io>
2024-06-09 21:42:59 +0200

    update

--- lib/GetCPM.cmake
+++ lib/GetCPM.cmake
@@ -1,10 +1,16 @@
+# GetCPM.cmake
+#
+# downloader for CPM
+
+set(CPM_TAG v0.39.0)
+set(CPM_TAG_HASH 66639bcac9dd2907b2918de466783554c1334446b9874e90d38e3778d404c2ef)
+
 file(
   DOWNLOAD
-  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.39.0/CPM.cmake
+  https://github.com/cpm-cmake/CPM.cmake/releases/download/${CPM_TAG}/CPM.cmake
   ${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake
-  EXPECTED_HASH SHA256=66639bcac9dd2907b2918de466783554c1334446b9874e90d38e3778d404c2ef
+  EXPECTED_HASH SHA256=${CPM_TAG_HASH}
 )
 include(${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake)
 
-
-# set(CPM_SOURCE_CACHE ${CMAKE_SOURCE_DIR}/vendored)
+message(STATUS "CPM Cache: ${CPM_SOURCE_CACHE}")
